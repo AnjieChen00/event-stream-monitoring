6c0ef1ea7b55336530026880d6f3b9cc3a79ca0b
Tianyi Yang <tianyi_yang@ucsb.edu>
2024-11-02 18:46:28 -0700

    support variable batch size and event ratio - T. Yang

--- eventgen_online_shopping.py
+++ eventgen_online_shopping.py
@@ -1,6 +1,9 @@
 import random
 import numpy as np
 import time
+import sys
+
+batch_size, conf_size = int(sys.argv[1]), sys.argv[2]
 t1 = time.time()
 # One day has 86400 seconds
 day = 86400
@@ -20,7 +23,7 @@ for uid in uids:
 	z1a = z0 + np.clip((int(xmax*(0.5+np.random.normal(0,1)))),1,xmax-1)
 	z1b = z0 + np.clip((int(xmax*(0.5+np.random.normal(0,1)))),1,xmax-1)
 	picking = {"event_type_name": "picking", 'order_id': str(oid), 'item_id': "cast iron", 'warehouse_id': str(wid), "event_time": z1a}
-	picking = {"event_type_name": "picking", 'order_id': str(oid), 'item_id': "chainmail cleaner", 'warehouse_id': str(wid), "event_time": z1b}	
+	picking = {"event_type_name": "picking", 'order_id': str(oid), 'item_id': "chainmail cleaner", 'package_id': str(pid), 'warehouse_id': str(wid), "event_time": z1b}	
 	z2 = max(z1a, z1b) + np.clip((int(ymax*(0.5+np.random.normal(0,1)))),1,ymax-1)
 	packing = {"event_type_name": "packing", 'order_id': str(oid), 'package_id': str(pid), 'item_id_quantity_mapping': default_item_id_quantity_mapping, 'warehouse_id': str(wid), "event_time": z2}
 	z3 = z2 + np.clip((int(ymax*(0.5+np.random.normal(0,1)))),1,ymax-1)
@@ -30,13 +33,16 @@ for uid in uids:
 	ship = {"event_type_name": "ship", 'package_id': str(pid), 'warehouse_id': str(wid), 'carrier_id': str(cid), 'tracking_number': str(tid), "event_time": z4+1}
 	z5 = z4 + np.clip((int(mmax*(0.5+np.random.normal(0,1)))),1,mmax-1)
 	deliver = {"event_type_name": "deliver", 'package_id': str(pid), 'carrier_id': str(cid), 'tracking_number': str(tid), "event_time": z5}
-	confirm_delivery = {"event_type_name": "confirm_delivery", 'package_id': str(pid), 'carrier_id': str(cid), 'tracking_number': str(tid), "event_time": z5+1}	
-	res.extend([place_order, picking, packing, assign_carrier, print_shipping_label, ship, deliver, confirm_delivery])
+	for j in range(int(conf_size)):
+		confirm_delivery = {"confirm_id": str(j), "event_type_name": "confirm_delivery", 'package_id': str(pid), 'carrier_id': str(cid), 'tracking_number': str(tid), "event_time": z5+1}
+		res.append(confirm_delivery)	
+	res.extend([place_order, picking, packing, assign_carrier, print_shipping_label, ship, deliver])
 
 res.sort(key=lambda x: x["event_time"])
 
-batchid, batch_size = 0, 100
-with open("online_shopping_events.txt", "a") as f:
+batchid= 0
+loc = f"./sample_events/online_shopping_events-batch_size_{batch_size}-conf_size_{conf_size}.txt"
+with open(loc, "a") as f:
 	batch = ""
 	batch_count = 0
 	for i in res:
@@ -62,11 +68,11 @@ Events:
 place_order = {'user_id': 'TEXT', 'order_id': 'TEXT', 'item_id_quantity_mapping': 'TEXT',
                                    'payment_method': 'TEXT', 'payment_amount': 'INTEGER',
                                    'payment_tracking_id': 'TEXT'}
-picking = {'order_id': 'TEXT', 'item_id': 'TEXT', 'warehouse_id': 'TEXT'}
+picking = {'order_id': 'TEXT', 'item_id': 'TEXT', 'warehouse_id': 'TEXT', 'package_id': 'TEXT'}
 packing = {'order_id': 'TEXT', 'package_id': 'TEXT', 'item_id_quantity_mapping': 'TEXT', 'warehouse_id': 'TEXT'}
 assign_carrier = {'package_id': 'TEXT', 'warehouse_id': 'TEXT', 'carrier_id': 'TEXT'}
 print_shipping_label = {'package_id': 'TEXT', 'warehouse_id': 'TEXT', 'carrier_id': 'TEXT', 'tracking_number': 'TEXT'}
 ship = {'package_id': 'TEXT', 'warehouse_id': 'TEXT', 'carrier_id': 'TEXT', 'tracking_number': 'TEXT'}
 deliver = {'package_id': 'TEXT', 'carrier_id': 'TEXT', 'tracking_number': 'TEXT'}
-confirm_delivery = {'package_id': 'TEXT', 'carrier_id': 'TEXT', 'tracking_number': 'TEXT'}
+confirm_delivery = {'confirm_id': 'TEXT', 'package_id': 'TEXT', 'carrier_id': 'TEXT', 'tracking_number': 'TEXT'}
 '''
